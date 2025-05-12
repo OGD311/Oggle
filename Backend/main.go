e540a35998bbf838341a42da58494823748fc8c1
OGD <114223604+OGD311@users.noreply.github.com>
2025-05-13 00:14:55 +0100

    Added basic code to read from an SQLITE DB for testing purposes (Must change later).

--- Backend/main.go
+++ Backend/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"net/http"
 
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 type Search struct {
 	Query string `form:"query"`
+}
+
+type Page struct {
+	Title string `json:"title"`
+	Description string `json:"description"`
+	URL string `json:"url"`
 }
 
 func main() {
@@ -30,6 +40,36 @@ func searchPage(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"msg": search.Query})
-	// ctx.String(http.StatusOK, search.Query)
+	pages, err := getPages(search.Query)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": pages})
+}
+
+func getPages(query string) ([]Page, error) {
+	db, err := sql.Open("sqlite", "file:oggle-db.db")
+    if err != nil {
+        return nil, err
+    }
+    defer db.Close()
+
+	stmt := `SELECT title, description, url FROM pages WHERE title like ? LIMIT 10`
+	rows, err := db.Query(stmt, "%"+query+"%")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []Page
+	for rows.Next() {
+		var p Page
+		if err := rows.Scan(&p.Title, &p.Description, &p.URL); err != nil {
+			return nil, err
+		}
+		results = append(results, p)
+	}
+
+	return results, nil
 }
