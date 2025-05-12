00a89da49b9e263d3641e89a94f614c0c08127fe
OGD <114223604+OGD311@users.noreply.github.com>
2025-05-12 23:28:38 +0100

    Added Gin CORS

--- Backend/main.go
+++ Backend/main.go
@@ -3,28 +3,33 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 type Search struct {
-	Query string `uri:"query" binding:"required"`
+	Query string `form:"query"`
 }
 
 func main() {
   router := gin.Default()
-
-  router.GET("/", func(ctx *gin.Context) {
-	ctx.JSON(http.StatusAccepted, gin.H{"msg": "home"})
-  })
   
-  router.GET("/:query", func(ctx *gin.Context)  {
+  router.Use(cors.Default())
+  
+  router.GET("/", searchPage)
+  
+  router.Run(":8080") 
+}
+
+func searchPage(ctx *gin.Context) {
 	var search Search
-	if err := ctx.ShouldBindUri(&search); err != nil {
-		ctx.JSON(http.StatusForbidden, gin.H{"msg" : err.Error()})
+
+	err := ctx.ShouldBind(&search)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	ctx.JSON(http.StatusAccepted, gin.H{"query": search.Query})
-	
-  })
-  router.Run(":8080") 
+
+	ctx.JSON(http.StatusOK, gin.H{"msg": search.Query})
+	// ctx.String(http.StatusOK, search.Query)
 }
